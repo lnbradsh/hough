214aa2ef20a58b3021c12323d99b1844364322fc
Ian Kenney <ian.kenney@asu.edu>
2017-02-20 14:46:36 -0700

    Added base setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+version = '0.1.0'
+
+setup(name="hough",
+      version=version,
+      description="FILL THIS IN",
+      author="Layne Bradshaw",
+      author_email="lnbradsh@asu.edu",
+      packages=find_packages(),
+      install_requires=['numpy>=1.6', 'scipy',
+      ],
+)
