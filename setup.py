c99563cfa58112aff0d0c0a4e39fed65d04c947f
Layne Bradshaw <lnbradsh@asu.edu>
2017-02-20 14:47:19 -0700

    updates setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ version = '0.1.0'
 
 setup(name="hough",
       version=version,
-      description="FILL THIS IN",
+      description="Implementation of the Hough algorithm",
       author="Layne Bradshaw",
       author_email="lnbradsh@asu.edu",
       packages=find_packages(),
