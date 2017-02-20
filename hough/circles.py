a084940893e1a96eda122bcc4d262d5e96a961e8
Layne Bradshaw <lnbradsh@asu.edu>
2017-02-20 14:40:47 -0700

    Added functions to generate plots of just circles, just ellipses, and both

--- hough/circles.py
+++ hough/circles.py
@@ -1,2 +1,22 @@
-def fit():
-    raise NotImplementedError
+def generate_circles(res,rmax,con=True):
+    """
+    res: Defines the step size used for theta_vals
+
+    rmax: Defines the maximum radius for the constructed circles
+
+    con: Used to decide whether or not to connect the points on the output graph. Default     value is True.
+
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(111,aspect='equal')
+    theta_vals = np.arange(0,2*np.pi+res,res)
+    xvals = np.cos(theta_vals)
+    yvals = np.sin(theta_vals)
+    r = np.arange(1,rmax+1)
+    for i in r:
+        if con:
+            plt.plot(i*xvals,i*yvals,'-o')
+        else:
+            plt.plot(i*xvals,i*yvals,'o')
+    ax.set_xlim(-rmax,rmax)
+    ax.set_ylim(-rmax,rmax)
---
+++ hough/ellipses.py
@@ -0,0 +1,21 @@
+def generate_ellipses(res,imax,jmax,con=True):
+    """
+
+    """
+    fig = plt.figure()
+    ax = plt.add_subplot(111,aspect='equal')
+    theta_vals = np.arange(0,2*np.pi+res,res)
+    xvals = np.cos(theta_vals)
+    yvals = np.sin(theta_vals)
+    i = np.arange(1,imax+1)
+    j = np.arange(1,jmax+1)
+    for a in i:
+        for b in j:
+            if a == b:
+                continue
+            elif con:
+                plt.plot(a*xvals,b*yvals,'-o')
+            else:
+                plt.plot(a*xvals,b*yvals,'o')
+    ax.set_xlim(-imax,imax)
+    ax.set_ylim(-jmax,jmax)
